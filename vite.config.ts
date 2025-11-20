04f333cb92456fc1319a42291d77a6efa0835a48
reservadearmas1cia12bpm <reservadearmas1cia12bpm@gmail.com>
2025-11-20 11:20:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      external: ['jszip'] // Garante que nenhum jszip seja incluído
+    }
+  },
+  define: {
+    'process.env': {}
   }
 });
