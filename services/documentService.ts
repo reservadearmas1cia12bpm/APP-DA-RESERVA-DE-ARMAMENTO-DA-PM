ddc0223d8666d1d82c6ec86f4a021f9d204943e1
reservadearmas1cia12bpm <reservadearmas1cia12bpm@gmail.com>
2025-11-20 10:16:55 -0300

    Update documentService.ts

--- services/documentService.ts
+++ services/documentService.ts
@@ -1,13 +1,25 @@
-import htmlDocx from "html-docx-js/dist/html-docx";
+export function exportToWord(htmlContent: string, fileName: string = "documento") {
+  const header = `
+    <html xmlns:o='urn:schemas-microsoft-com:office:office'
+          xmlns:w='urn:schemas-microsoft-com:office:word'
+          xmlns='http://www.w3.org/TR/REC-html40'>
+    <head><meta charset="utf-8"></head><body>
+  `;
 
-export function exportToWord(html: string, filename: string) {
-  const docxBlob = htmlDocx.asBlob(html);
+  const footer = "</body></html>";
 
-  const url = URL.createObjectURL(docxBlob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = filename + ".docx";
-  a.click();
+  const sourceHTML = header + htmlContent + footer;
+
+  const blob = new Blob(['\ufeff', sourceHTML], {
+    type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  });
+
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = fileName + ".docx";
+  link.click();
 
   URL.revokeObjectURL(url);
 }
